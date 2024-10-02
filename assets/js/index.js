37a23a9091135a85b0c21654d1702bf2cc2f6cbd
dagalvisb <daniel.galvis42587@ucaldas.edu.co>
2024-10-01 21:35:36 -0500

    parcial parcial

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,39 @@
-fetch('data/taller.json')
+fetch('data/datos.json')
     .then((response) => response.json())
     .then(data => {
-        const tableContainer = document.getElementById("tabla");
-        let tabla = '';
-        data.computadores.forEach(function(informacion) {
-            tabla += `<tr>
-                        <td>${informacion.nombre}</th>
-                        <td>${informacion.descripcion}</td>
-                        <td>${informacion.precio}</td>
-                        <td>${informacion.imagen}</td>
-                    </tr>`
-        });
-        tableContainer.innerHTML= tabla;
+
+        const cardContainer = document.getElementById("cards");
+        let cards = '';
+        
+        data.productos.forEach(function(informacion) {
+            let rec = '';
+            informacion.reseñas.forEach(function(reseña){
+                
+                rec += `<h1>${reseña.usuario}</h1>
+                    <p>${reseña.comentario}</p>
+                    <p>${reseña.calificacion}</p>
+                    <p>${reseña.fecha}</p>`
+            });
+            
+
+            cards +=
+            ` <div class="col">
+                <div class="card">
+                <img src="${informacion.imagenes[0]}" class="card-img-top" alt="...">
+                <img src="${informacion.imagenes[1]}" class="card-img-top" alt="...">
+                <img src="${informacion.imagenes[2]}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">${informacion.nombre}</h5>
+                        <h5 class="card-title">${informacion.categoria}</h5>
+                        <p class="card-text">${informacion.descripcion}</p>
+                        <h1 class="card-title">${informacion.precio}</h1>
+                        <div>${rec}</div>
+                    </div>
+                </div>
+            </div>`
+            });
+        cardContainer.innerHTML= cards;
+
 
         const tituloContainer = document.getElementById("titulo");
         const titulo = data.titulo_pagina;
@@ -32,5 +54,16 @@ fetch('data/taller.json')
 
         const direccionTienda = document.getElementById("direccion_tienda");
         direccionTienda.innerHTML = "Direccion:" + data.datos_tienda.direccion;
+
+        const horarioTienda = document.getElementById("horario_tienda")
+        
+        let horario = '';
+        horario =
+        `<p>Lunes a Viernes: ${data.datos_tienda.horario_atencion.lunes_a_viernes}</p>
+        <p>Sábados: ${data.datos_tienda.horario_atencion.sabados}</p>
+        <p>Domingos: ${data.datos_tienda.horario_atencion.domingos}</p>`
+        
+        horarioTienda.innerHTML = horario;
+        
 
     })
